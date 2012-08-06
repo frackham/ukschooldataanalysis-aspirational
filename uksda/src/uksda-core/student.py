c485400a005065029877e0ca90f962b40830bc2e
Fraser Rackham <frackham@googlemail.com>
2012-08-06 20:33:37 +0100

    Initial student class file.

---
+++ uksda/src/uksda-core/student.py
@@ -0,0 +1,17 @@
+'''
+Created on 6 Aug 2012
+
+@author: Fraser
+'''
+
+class Student(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
